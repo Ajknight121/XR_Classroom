5137115ebcaf5829f0c694c9c355230c068f9c94
VShallVanish <vmattr2@uic.edu>
2025-02-19 15:14:05 -0600

    made walls slide down script

---
+++ Assets/goDown.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goDown : MonoBehaviour
+{
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > 1f)
+            transform.position = new Vector3(transform.position.x, transform.position.y - speed * Time.deltaTime, transform.position.z);
+    }
+}

6528e2e3837c7f7926c81b73e4be6af2d931f255
sofi-blazhko <sblaz2@uic.edu>
2025-03-05 13:08:44 -0600

    collisions

---
+++ Assets/materals/CollisionsScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionsScript : MonoBehaviour
+{
+
+    public AudioClip mySound;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<AudioSource>().PlayOneShot(mySound);
+            Debug.Log("collide & play sound");
+        }
+    }
+}

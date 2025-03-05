ffb2a677ea0017888a088b5f31a52a96b1bfe4a3
Ajknight121 <97642775+Ajknight121@users.noreply.github.com>
2025-03-05 01:26:10 -0600

    Approaching locations in networking added

---
+++ Assets/MoveToTarget.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToTarget : MonoBehaviour
+{
+    public Transform from;
+    public Transform to;
+    public float distancePerFrame;
+    public bool isMoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = from.position;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving) {
+            Vector3 direction = (to.position - from.position).normalized;
+            float step = distancePerFrame * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, to.position, step);
+        }
+    }
+}

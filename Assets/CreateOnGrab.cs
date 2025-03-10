3eaf93a54108c6067450d03177e289ec97f0b66f
Ajknight121 <97642775+Ajknight121@users.noreply.github.com>
2025-03-10 13:35:09 -0500

    grabbable bool now public, create object on grab

---
+++ Assets/CreateOnGrab.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateOnGrab : MonoBehaviour
+{
+    public float delay;
+    public float maxDelay;
+    public GrabbableObject obj;
+    public GameObject spawnedPrefab;
+    public Transform spawnLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = GetComponent<GrabbableObject>();
+        Debug.Log(obj);
+        delay = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (obj.grabbed && delay <= 0) {
+            GameObject newInstance;
+            newInstance = Instantiate(spawnedPrefab, spawnLocation.position,
+            spawnLocation.rotation);
+            newInstance.GetComponent<Rigidbody>().AddForce(spawnLocation.forward * 50);
+            
+            delay = maxDelay;
+        }
+        delay -= 1 * Time.deltaTime;
+    }
+}

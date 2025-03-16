0439a94660af40eca08c1f9905103035af2ad495
Ajknight121 <97642775+Ajknight121@users.noreply.github.com>
2025-03-16 16:08:41 -0500

    don't use animator for now

--- Assets/MoveToTarget.cs
+++ Assets/MoveToTarget.cs
@@ -10,15 +10,19 @@ public class MoveToTarget : MonoBehaviour
     public bool isMoving;
     public bool isData;
     public bool played;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         played = false;
-        GetComponent<Animator>().StopPlayback();
 
         if (!isData)
         {
+            animator = GetComponent<Animator>();
+            if (animator) {
+                animator.StopPlayback();
+            }
             transform.position = from.position;
         }
     }
@@ -29,15 +33,15 @@ public class MoveToTarget : MonoBehaviour
     {
         if (isMoving)
         {
-            // Vector3 direction = (to.position - from.position).normalized;
-            // float step = distancePerFrame * Time.deltaTime;
-            // transform.position = Vector3.MoveTowards(transform.position, to.position, step);
-            // // if (isData) {
-            // //     transform.position.y = -6;
-            // // }
+            Vector3 direction = (to.position - from.position).normalized;
+            float step = distancePerFrame * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, to.position, step);
+            // if (isData) {
+            //     transform.position.y = -6;
+            // }
             if (played)
             {
-                GetComponent<Animator>().Play("New Animation", -1, 0f);
+                GetComponent<Animator>().Play("JapanView", -1, 0f);
             }
         }
     }

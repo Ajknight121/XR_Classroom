cad1735e6a1bfd81ce7a73f23aa6d50b662e6f96
V <vmattr2@uic.edu>
2025-04-07 20:39:17 -0500

    update space demo

--- Assets/changeSceneOnGrab.cs
+++ Assets/changeSceneOnGrab.cs
@@ -7,6 +7,7 @@ public class changeSceneOnGrab : MonoBehaviour
 {
     private GrabbableObject grabTarget;
     public string sceneTitle;
+    public Scene scene;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +18,10 @@ public class changeSceneOnGrab : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (grabTarget.grabbed) {
+        if (grabTarget.grabbed)
+        {
             SceneManager.LoadScene(sceneTitle);
+            
         }
     }
 }
--- Assets/goDown.cs
+++ Assets/goDown.cs
@@ -6,10 +6,10 @@ public class goDown : MonoBehaviour
 {
     public float speed = 1f;
     public float stopYValue = -5f;
+    public float moveXValue = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -19,5 +19,11 @@ public class goDown : MonoBehaviour
         if (transform.position.y <= stopYValue)
             return;
         transform.position = new Vector3(transform.position.x, transform.position.y - speed * Time.deltaTime, transform.position.z);
+
+        if (moveXValue > 0 || moveXValue < 0)
+        {
+            transform.position = new Vector3(transform.position.x + moveXValue * Time.deltaTime, transform.position.y, transform.position.z);
+            moveXValue -= moveXValue * Time.deltaTime;
+        }
     }
 }
